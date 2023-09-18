a7ed3e9540b2582bfb1014d74599c9eb152735f4
Otto Hother Sørensen <ottohothersorensen@Ottos-MacBook-Pro.local>
2023-09-18 23:03:12 +0200

    Hello

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
-const router = require("./router");
+const router = require("./router/router");
+const { pathToFileURL } = require('url');
+
+app.use('router', router);
+app.use(express.static("../FRONTEND")); 
+app.use(express.urlencoded({extended : true}));
 
 const PORT = 3000;
 const HOST = '127.0.0.1'
 
-app.use('router', router);
+const filePath = path.join(__dirname, 'FRONTEND', 'index.html')
 
 app.get("/", (req, res) => {
 
@@ -15,12 +21,27 @@ app.get("/", (req, res) => {
 })
 
 
+app.get('/static', (req, res) => {
+
+    res.sendFile(filePath)
+    
+})
+
+
 app.get("/new", (req, res) => {
 
     res.send({joe : "And the Juice"})
+
+
+})
+
+app.get("/dir", (req, res) => {
+
+    res.send(__dirname)
 
 })
 
 app.listen(PORT, HOST,() => {
     console.log(`Server is now running on ${PORT}`)
+
 })  
