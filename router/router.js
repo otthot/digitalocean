95ecd3a7801c2d7321ee5ba9518fa31c2190dd1b
Otto Hother Sørensen <ottohothersorensen@Ottos-MacBook-Pro.local>
2023-09-20 09:25:26 +0200

    new router

--- router/router.js
+++ router/router.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const Router = express.Router()
 
+const users =  {1 : {name : "John"}, 2 : {name : "Tim"}, 3 : {name : "Anton"}}
 
-Router.get('/', (req, res) =>(
 
-    res.send("Hello!")
+Router.get('/:id', (req, res) =>(
+
+    res.send(users)
 
 ))
 
